d2b2f2e561323b8afe68f9dbefba01f8c7ed84ab
github username <github email address>
2018-02-19 17:59:06 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,54 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
   }
+
+  setUsername = event => {
+    this.setState({
+      username: event.target.value
+    });
+  };
+
+  setPassword = event => {
+    this.setState({
+      password: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if (this.state.username.length > 0 && this.state.password.length > 0) {
+      this.props.onSubmit();
+    }
+  };
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input
+              id="test-username"
+              type="text"
+              value={this.state.value}
+              onChange={this.setUsername}
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input
+              id="test-password"
+              type="password"
+              value={this.state.value}
+              onChange={this.setPassword}
+            />
           </label>
         </div>
         <div>
--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,14 +4,64 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: "",
+      showError: true
+    };
   }
+
+  handleChange = event => {
+    this.setState({
+      value: event.target.value
+    });
+    let poem = this.state.value.split("\n");
+    if (poem.length === 3) {
+      let line1 = poem[0].split(" ");
+      let line2 = poem[1].split(" ");
+      let line3 = poem[2].split(" ");
+
+      line1 = line1.filter(word => {
+        return word !== " ";
+      });
+      line2 = line2.filter(word => {
+        return word !== " ";
+      });
+      line3 = line3.filter(word => {
+        return word !== " ";
+      });
+
+      if (line1.length === 5 && line2.length === 3 && line3.length === 5) {
+        this.setState({
+          showError: false
+        });
+      } else {
+        this.setState({
+          showError: true
+        });
+      }
+    } else {
+      this.setState({
+        showError: true
+      });
+    }
+  };
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{ color: "red" }}>
+        <textarea
+          rows="3"
+          cols="60"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <div
+          id="poem-validation-error"
+          style={{
+            color: "red",
+            visibility: this.state.showError ? "visible" : "hidden"
+          }}
+        >
           This poem is not written in the right structure!
         </div>
       </div>
@@ -20,3 +70,9 @@ class PoemWriter extends React.Component {
 }
 
 export default PoemWriter;
+
+// The first line has five words.
+// The second line has three words.
+// The third line has five words.
+// Be sure to account for users accidentally adding too many spaces! This means that ' I am a furry dog ' is not a valid first line of the poem.
+// Make sure the error message is still showing if the user deletes all of their content from the textarea box
